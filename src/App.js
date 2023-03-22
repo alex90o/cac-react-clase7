60bbfdf7033b971a0beda2d4f6c0003431be72ee
Alexis <alex10o@hotmail.com>
2023-03-22 17:50:29 -0300

    commit 1

--- src/App.js
+++ src/App.js
@@ -7,8 +7,16 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Clase 7 Primer App React Alexis Cabrera 2023
         </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
         <a
           className="App-link"
           href="https://reactjs.org"
